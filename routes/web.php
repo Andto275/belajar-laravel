2858decc2287c0934fb68543f66139a85f71ba10
Suharto <suharto@stkippgrisumenep.ac.id>
2021-06-12 15:19:09 +0700

    setup breeze dan laravel-permission

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\GelombangController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::middleware('role:admin')->get('/gelombang', [GelombangController::class, 'index'])->name('gelombang');
+});
+
+
+require __DIR__ . '/auth.php';
